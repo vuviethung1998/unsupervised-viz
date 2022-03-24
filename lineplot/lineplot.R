d2184249e1ffcce43ec287276514aa6da8b211e3
Hung Vu <vuviethung.98.hust@gmail.com>
2022-03-24 11:35:11 +0700

    update

---
+++ lineplot/lineplot.R
@@ -0,0 +1,51 @@
+##### read data 
+data<-read.csv("../data/Station_24_cut_bilstm.csv",header=T,sep=",")
+data
+##### define data range 
+
+
+max_x <- max(data$X) ## minimum x 
+min_x <- min(data$X)  ## maximum x
+max_y <- max(data$Groundtruth, data$Predict)  ## minimum y
+min_y <- min(data$Groundtruth, data$Predict)  ## maximum x
+### max_e <- max(data$inma_e, data$qlearning_e, data$nocharge_e)  ### maximum error 
+
+##### export eps file 
+setEPS()
+postscript("Station24_lineplot_IDW_BILSTM.eps",height=6.5, width=6.5)
+
+##### define margins 
+par(mar=c(5, 5, 1, 1) + 0.1)
+par(mgp = c(3, 1, 0))
+
+##### define colors  "blue","darkgreen", "purple", "red"
+plot_colors <-  c( "blue", "red")
+
+##### define plot area 
+##### xlab, ylab are the titles of the x-axis, y-axis 
+##### use expression for math formulars 
+##### at = seq(200, 400, by = 50) is for the ticks. ommit this if you want to use the default value 
+##### cex is the size 
+plot(c(min_x,max_x),c(min_y, max_y+10), type="n", xaxt="n", cex.lab = 1.5, cex.axis=1.5, xlab="Date", ylab=expression(paste("PM2.5 (", mu, "g", "/", m^3,")")))
+axis(1, at = seq(0, 200, by = 20), las=1,  cex.axis=1.5)
+
+##### to draw lines 
+##### pch is the type of point; lty is the type of line; col is the color; lwd is the weight of the line, cex is the size of the point. 
+lines(data$X, data$Groundtruth, type="l", pch=0, lty=1, col=plot_colors[1], lwd=2.5, cex = 1.6)
+lines(data$X,data$Predict, type="l", pch=1, lty=1, col=plot_colors[2], lwd=2.5, cex = 1.6)
+# lines(data$node, data$inma, type="o", pch=2, lty=1, col=plot_colors[3], lwd=2.5, cex = 1.6)
+# lines(data$node, data$nocharge, type="o", pch=5, lty=1, col=plot_colors[4], lwd=2.5, cex = 1.6)
+
+###### to draw condidence intervals
+#arrows(x0=data$sensor_num, y0=data$galm-data$galm_e, x1=data$sensor_num, y1= data$galm+data$galm_e, code=3, angle=90, length = 0.05, col=plot_colors[1])
+#arrows(x0=data$sensor_num, y0=data$grlm-data$grlm_e, x1=data$sensor_num, y1= data$grlm+data$grlm_e, code=3, angle=90, length = 0.05, col=plot_colors[2])
+#arrows(x0=data$sensor_num, y0=data$inma-data$inma_e, x1=data$sensor_num, y1= data$inma+data$inma_e, code=3, angle=90, length = 0.05, col=plot_colors[3])
+#arrows(x0=data$sensor_num, y0=data$no_charge-data$inma_e, x1=data$sensor_num, y1= data$inma+data$inma_e, code=3, angle=90, length = 0.05, col=plot_colors[3])
+#arrows(x0=data$sensor_num, y0=data$pso-data$pso_e, x1=data$sensor_num, y1= data$pso + data$pso_e, code=3, angle=90, length = 0.05, col=plot_colors[4])
+#legend(bty = "n","topright", c("GALM","GRLM","INMA", "HPSOGA)","No charging"),pch=c(0,2,5,8), lty=c(1,1,1,1,1), col=plot_colors, cex=1.6, x.intersp=1, y.intersp=1.5)
+
+###### to draw the legend
+legend(bty = "n","topright", c("Air Quality", "IDW-BiLSTM"),pch=c(0,1,2,5), lty=c(1,1,1,1), col=plot_colors, lwd=1, cex=1.6, x.intersp=1, y.intersp=1.5)
+
+dev.off()
+
--- location-viz/location-viz.R
+++ location-viz/location-viz.R
@@ -1,39 +1,88 @@
-#install.packages("ggplot2")
-install.packages("gridExtra")
-install.packages("cowplot")
-library("cowplot")
-library("gridExtra")
-library(ggplot2)
+# read the data 
+data<-read.csv("../data/location_viz_test3.csv",header=T,sep=",")
 
-get_legend<-function(myggplot){
-  tmp <- ggplot_gtable(ggplot_build(myggplot))
-  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
-  legend <- tmp$grobs[[leg]]
-  return(legend)
-}
+# define the min, max of x and y 
+max_x <- max(data$longitude)
+min_x <- min(data$longitude)
+max_y <- max(data$latitude)
+min_y <- min(data$latitude)
+
+# export to eps file 
+setEPS()
+postscript("test3_distribution.eps",height=6, width=6)
+
+# define the margin of the figure 
+par(mar=c(5, 5, 1, 1) + 0.1)
+par(mgp = c(3, 1, 0))
+
+# define the color 
+plot_colors <-  c("#FF0000", "#00FF00","#0000FF")
+
+# define the plot range 
+plot(c(min_x,max_x),c(min_y,max_y), type="n", cex.lab = 1.5, cex.axis=1.5, xlab="Longitude", ylab="Latitude")
+
+# draw line, 
+# pch: type of point, lty: type of line 
+# col: color of line and point 
+# cex: size of point, lws: width of line 
+
+s1 <- data[data$c == "#FF0000",]
+s2 <- data[data$c == "#00FF00",]
+s3 <- data[data$c == "#0000FF",]
+s1$longitude
+
+lines(s1$longitude, s1$latitude, type="o", pch=17, lty=0, col=plot_colors[1], cex=.8, lwd=1.5)
+lines(s2$longitude, s2$latitude, type="o", pch=8, lty=0, col=plot_colors[2], cex=.8, lwd=1.5)
+lines(s3$longitude, s3$latitude, type="o", pch=16, lty=0, col=plot_colors[3], cex=.8, lwd=1.5)
+abline(h=0, col="blue")
+
+#draw the legend 
+legend(bty = "o","topright",c("Train stations","Validation stations", "Test stations"),pch=c(8,16,17), lty=c(0,0,0), col=plot_colors, cex=1.2, x.intersp=1, y.intersp=1.5)
+dev.off()
+
+# # draw the confidence intervals which are vertical segments at every point. 
+# arrows(x0=data$area, y0=data$bcp_av-data$bcp_err, x1=data$area, y1= data$bcp_av+data$bcp_err, code=3, angle=90, length = 0.05, col=plot_colors[1])
+# arrows(x0=data$area, y0=data$hach_av-data$hach_err, x1=data$area, y1= data$hach_av+data$hach_err, code=3, angle=90, length = 0.05, col=plot_colors[2])
+# arrows(x0=data$area, y0=data$hpa_av-data$hpa_err, x1=data$area, y1= data$hpa_av+data$hpa_err, code=3, angle=90, length = 0.05, col=plot_colors[3])
 
 
-locations1<-read.csv("../data/location_viz_test1.csv")
-locations2<-read.csv("../data/location_viz_test2.csv")
-locations3<-read.csv("../data/location_viz_test3.csv")
-locations4<-read.csv("../data/location_viz_test4.csv")
-print(locations1)
-
-# Create subplots
-plot1 <- ggplot(locations1, aes(y=latitude, x=longitude, color=label)) + geom_point() 
-plot2 <- ggplot(locations2, aes(y=latitude, x=longitude, color=label)) + geom_point() + 
-  theme(legend.position="none")
-plot3 <- ggplot(locations3, aes(y=latitude, x=longitude, color=label)) + geom_point() + 
-  theme(legend.position="none")
-plot4 <- ggplot(locations4, aes(y=latitude, x=longitude, color=label)) + geom_point() + 
-  theme(legend.position="none")
-# 2. Save the legend
-#+++++++++++++++++++++++
-legend <- get_legend(plot1)
-# 3. Remove the legend from the box plot
-#+++++++++++++++++++++++
-plot1 <- plot1 + theme(legend.position="none")
-
-pic <- grid.arrange(plot1, plot2, plot3, plot4, legend, ncol=5, widths=c(2.3, 2.3, 2.3, 2.3, 0.8))
-
-ggsave(file="../output/loc_viz.eps", pic)
+# #install.packages("ggplot2")
+# #install.packages("gridExtra")
+# #install.packages("cowplot")
+# library("cowplot")
+  # library("gridExtra")
+# library(ggplot2)
+# 
+# get_legend<-function(myggplot){
+#   tmp <- ggplot_gtable(ggplot_build(myggplot))
+#   leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+#   legend <- tmp$grobs[[leg]]
+#   return(legend)
+# }
+# 
+# 
+# locations1<-read.csv("../data/location_viz_test1.csv")
+# locations2<-read.csv("../data/location_viz_test2.csv")
+# locations3<-read.csv("../data/location_viz_test3.csv")
+# #locations4<-read.csv("../data/location_viz_test4.csv")
+# print(locations1)
+# 
+# # Create subplots
+# plot1 <- ggplot(locations1, aes(y=latitude, x=longitude, color=label)) + geom_point() 
+# plot2 <- ggplot(locations2, aes(y=latitude, x=longitude, color=label)) + geom_point() + 
+#   theme(legend.position="none")
+# plot3 <- ggplot(locations3, aes(y=latitude, x=longitude, color=label)) + geom_point() + 
+#   theme(legend.position="none")
+# # plot4 <- ggplot(locations4, aes(y=latitude, x=longitude, color=label)) + geom_point() + 
+# #   theme(legend.position="none")
+# # 2. Save the legend
+# #+++++++++++++++++++++++
+# legend <- get_legend(plot1)
+# # 3. Remove the legend from the box plot
+# #+++++++++++++++++++++++
+# plot1 <- plot1 + theme(legend.position="none")
+# 
+# pic <- grid.arrange(plot1, plot2, plot3, legend, ncol=5, widths=c(2.3, 2.3, 2.3, 2.3, 0.8))
+# 
+# ggsave(file="../output/loc_viz.eps", pic)
+# 
